0405041e3d8a7e87e3c1f173b341f9a5363a0e57
guthember <guthember@gmail.com>
2020-10-22 09:27:46 +0200

    Auto és Igeny osztály létrehozása

---
+++ TelekocsiForm/Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekocsiForm
+{
+  class Auto
+  {
+    public string Indulas { get; private set; }
+    public string Cel { get; private set; }
+    public string Rendszam { get; private set; }
+    public string Tel { get; private set; }
+    public int Ferohely { get; private set; }
+    public string Utvonal { get; private set; }
+
+    public Auto(string indulas, string cel, string rendszam, string tel, int ferohely)
+    {
+      Indulas = indulas;
+      Cel = cel;
+      Rendszam = rendszam;
+      Tel = tel;
+      Ferohely = ferohely;
+      Utvonal = Indulas + "-" + Cel;
+    }
+  }
+}
---
+++ TelekocsiForm/Igeny.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekocsiForm
+{
+  class Igeny
+  {
+    public string Azonosito { get; private set; }
+    public string Indulas { get; private set; }
+    public string Cel { get; private set; }
+    public int Szemelyek { get; private set; }
+    public string Utvonal { get; private set; }
+
+    public Igeny(string azonosito, string indulas, string cel, int szemelyek)
+    {
+      Azonosito = azonosito;
+      Indulas = indulas;
+      Cel = cel;
+      Szemelyek = szemelyek;
+      Utvonal = Indulas + "-" + Cel;
+    }
+
+    public int VanAuto(List<Auto> autok)
+    {
+      int i = 0;
+      while (i < autok.Count &&
+        !(
+            Utvonal == autok[i].Utvonal &&
+            Szemelyek <= autok[i].Ferohely
+        )
+      )
+      {
+        i++;
+      }
+
+      if (i < autok.Count)
+      {
+        return i;
+      }
+      else
+      {
+        return -1;
+      }
+    }
+  }
+}

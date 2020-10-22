8e0f2032ed1ec7a27c445a18cf92a39707f7fab5
guthember <guthember@gmail.com>
2020-10-22 10:37:36 +0200

    4. feladatig

--- TelekocsiForm/Form1.Designer.cs
+++ TelekocsiForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TelekocsiForm
 {
-  partial class Form1
+  partial class frmFo
   {
     /// <summary>
     /// Required designer variable.
@@ -28,13 +28,93 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.btnBeolvasas = new System.Windows.Forms.Button();
+      this.lbKimenet = new System.Windows.Forms.ListBox();
+      this.btnSecond = new System.Windows.Forms.Button();
+      this.btnThird = new System.Windows.Forms.Button();
+      this.btnFourth = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnBeolvasas
+      // 
+      this.btnBeolvasas.Location = new System.Drawing.Point(12, 12);
+      this.btnBeolvasas.Name = "btnBeolvasas";
+      this.btnBeolvasas.Size = new System.Drawing.Size(116, 29);
+      this.btnBeolvasas.TabIndex = 0;
+      this.btnBeolvasas.Text = "Beolvasás";
+      this.btnBeolvasas.UseVisualStyleBackColor = true;
+      this.btnBeolvasas.Click += new System.EventHandler(this.btnBeolvasas_Click);
+      // 
+      // lbKimenet
+      // 
+      this.lbKimenet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.lbKimenet.FormattingEnabled = true;
+      this.lbKimenet.ItemHeight = 16;
+      this.lbKimenet.Location = new System.Drawing.Point(143, 12);
+      this.lbKimenet.Name = "lbKimenet";
+      this.lbKimenet.Size = new System.Drawing.Size(429, 276);
+      this.lbKimenet.TabIndex = 1;
+      // 
+      // btnSecond
+      // 
+      this.btnSecond.Enabled = false;
+      this.btnSecond.Location = new System.Drawing.Point(12, 47);
+      this.btnSecond.Name = "btnSecond";
+      this.btnSecond.Size = new System.Drawing.Size(116, 30);
+      this.btnSecond.TabIndex = 2;
+      this.btnSecond.Text = "2. Feladat";
+      this.btnSecond.UseVisualStyleBackColor = true;
+      this.btnSecond.Click += new System.EventHandler(this.btnSecond_Click);
+      // 
+      // btnThird
+      // 
+      this.btnThird.Enabled = false;
+      this.btnThird.Location = new System.Drawing.Point(12, 83);
+      this.btnThird.Name = "btnThird";
+      this.btnThird.Size = new System.Drawing.Size(116, 32);
+      this.btnThird.TabIndex = 3;
+      this.btnThird.Text = "3. Feladat";
+      this.btnThird.UseVisualStyleBackColor = true;
+      this.btnThird.Click += new System.EventHandler(this.btnThird_Click);
+      // 
+      // btnFourth
+      // 
+      this.btnFourth.Enabled = false;
+      this.btnFourth.Location = new System.Drawing.Point(12, 121);
+      this.btnFourth.Name = "btnFourth";
+      this.btnFourth.Size = new System.Drawing.Size(116, 32);
+      this.btnFourth.TabIndex = 4;
+      this.btnFourth.Text = "4. Feladat";
+      this.btnFourth.UseVisualStyleBackColor = true;
+      this.btnFourth.Click += new System.EventHandler(this.btnFourth_Click);
+      // 
+      // frmFo
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(584, 361);
+      this.Controls.Add(this.btnFourth);
+      this.Controls.Add(this.btnThird);
+      this.Controls.Add(this.btnSecond);
+      this.Controls.Add(this.lbKimenet);
+      this.Controls.Add(this.btnBeolvasas);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.Name = "frmFo";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Telekocsi";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button btnBeolvasas;
+    private System.Windows.Forms.ListBox lbKimenet;
+    private System.Windows.Forms.Button btnSecond;
+    private System.Windows.Forms.Button btnThird;
+    private System.Windows.Forms.Button btnFourth;
   }
 }
 
--- TelekocsiForm/Form1.cs
+++ TelekocsiForm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,104 @@ using System.Windows.Forms;
 
 namespace TelekocsiForm
 {
-  public partial class Form1 : Form
+  public partial class frmFo : Form
   {
-    public Form1()
+    private List<Auto> autok = new List<Auto>();
+    private List<Igeny> igenyek = new List<Igeny>();
+
+    public frmFo()
     {
       InitializeComponent();
+      lbKimenet.Items.Clear();
+    }
+
+    private void btnBeolvasas_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        StreamReader a = new StreamReader("autok.csv");
+        a.ReadLine();
+        while (!a.EndOfStream)
+        {
+          string[] adat = a.ReadLine().Split(';');
+          autok.Add(new Auto(adat[0], adat[1], adat[2], adat[3], Convert.ToInt32(adat[4])));
+        }
+        a.Close();
+
+        StreamReader i = new StreamReader("igenyek.csv");
+        i.ReadLine();
+        while (!i.EndOfStream)
+        {
+          string[] adat = i.ReadLine().Split(';');
+          igenyek.Add(new Igeny(adat[0], adat[1], adat[2], Convert.ToInt32(adat[3])));
+        }
+        i.Close();
+
+        btnSecond.Enabled = true;
+        btnBeolvasas.Enabled = false;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+
+    private void btnSecond_Click(object sender, EventArgs e)
+    {
+      lbKimenet.Items.Add("2. feladat");
+      lbKimenet.Items.Add($"   {autok.Count} autós hirdet fuvart");
+      btnSecond.Enabled = false;
+      btnThird.Enabled = true;
+    }
+
+    private void btnThird_Click(object sender, EventArgs e)
+    {
+      lbKimenet.Items.Clear();
+
+      lbKimenet.Items.Add("3. feladat");
+      int ferohely = 0;
+      foreach (var a in autok)
+      {
+        if (a.Utvonal == "Budapest-Miskolc")
+        {
+          ferohely += a.Ferohely;
+        }
+      }
+
+      lbKimenet.Items.Add($"   Összesen {ferohely} férőhelyet hirdettek az autósok Budapestről Miskolcra");
+      btnThird.Enabled = false;
+      btnFourth.Enabled = true;
+    }
+
+    private void btnFourth_Click(object sender, EventArgs e)
+    {
+      int max = 0;
+      string utv = "";
+
+      var utvonalak = from a in autok
+                      orderby a.Utvonal
+                      group a by a.Utvonal into temp
+                      select temp;
+
+      foreach (var ut in utvonalak)
+      {
+        int fh = ut.Sum(x => x.Ferohely);
+        if (max < fh)
+        {
+          max = fh;
+          utv = ut.Key;
+        }
+      }
+
+      lbKimenet.Items.Clear();
+      lbKimenet.Items.Add("4. feladat");
+      lbKimenet.Items.Add("A legtöb férőhelyet");
+      lbKimenet.Items.Add($"({max})");
+      lbKimenet.Items.Add("a");
+      lbKimenet.Items.Add($"{utv} útvonalon");
+      lbKimenet.Items.Add("ajánlották fel a hirdetők");
+
+      btnFourth.Enabled = false;
     }
   }
 }
